094bcb1aaa2dcf8ccfedd3b0c9278c88de9380ae
profchaudhry <lectures.uni@gmail.com>
2026-02-23 22:07:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import streamlit as st
 from ui.styles import load_styles
 from ui.login import login_page
-from ui.admin import admin_console
-from ui.faculty import faculty_console
-from ui.student import student_console
+from core.router import route
+import core.logger
 
-st.set_page_config(
-    page_title="SylemaX",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+st.set_page_config(page_title="SylemaX", layout="wide")
 
 load_styles()
 
@@ -22,16 +17,4 @@ if "role" not in st.session_state:
 if st.session_state.user is None:
     login_page()
 else:
-    if st.sidebar.button("Logout"):
-        st.session_state.user = None
-        st.session_state.role = None
-        st.rerun()
-
-    role = st.session_state.role
-
-    if role == "admin":
-        admin_console()
-    elif role == "faculty":
-        faculty_console()
-    elif role == "student":
-        student_console()
+    route(st.session_state.role)
