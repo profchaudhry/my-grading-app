f4e7ed126964e84ad263f90b7640daeb3782d6cb
profchaudhry <lectures.uni@gmail.com>
2026-02-23 22:05:57 +0500

    Update dashboard.py

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -1,29 +1,18 @@
 import streamlit as st
-from services.faculty_service import get_profile
-from services.student_service import get_student_profile
+from services.faculty_service import FacultyService
+from services.student_service import StudentService
 
 def render_dashboard():
 
-    user = st.session_state.user
     role = st.session_state.role
+    user = st.session_state.user
 
-    if role == "student":
-        data = get_student_profile(user.id)
+    if role == "faculty":
+        data = FacultyService.get_profile(user.id)
     else:
-        data = get_profile(user.id)
+        data = StudentService.get_profile(user.id)
 
     st.title("Dashboard")
-    st.markdown("### You are logged in as:")
 
-    st.markdown('<div class="card">', unsafe_allow_html=True)
-
-    st.markdown(f"**Role:** {role.capitalize()}")
-
-    if role == "student":
-        st.markdown(f"**Name:** {data.get('first_name','')} {data.get('last_name','')}")
-        st.markdown(f"**Enrollment Number:** {data.get('enrollment','')}")
-    else:
-        st.markdown(f"**Name:** {data.get('first_name','')} {data.get('last_name','')}")
-        st.markdown(f"**Email Address:** {data.get('email')}")
-
-    st.markdown('</div>', unsafe_allow_html=True)
+    st.markdown(f"**Role:** {role}")
+    st.markdown(f"**Name:** {data.get('first_name','')} {data.get('last_name','')}")
