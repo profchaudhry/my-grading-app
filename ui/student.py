0cd061984d43f6c8103558be460d4db30e1adf55
profchaudhry <lectures.uni@gmail.com>
2026-02-23 22:05:11 +0500

    Update student.py

--- ui/student.py
+++ ui/student.py
@@ -1,14 +1,11 @@
-import streamlit as st
+from core.layout import base_console
+from core.guards import require_role
 from ui.dashboard import render_dashboard
 
+@require_role(["student"])
 def student_console():
 
-    st.sidebar.title("Student Panel")
-
-    menu = st.sidebar.radio(
-        "",
-        ["Dashboard"]
-    )
+    menu = base_console("Student Panel", ["Dashboard"])
 
     if menu == "Dashboard":
         render_dashboard()
