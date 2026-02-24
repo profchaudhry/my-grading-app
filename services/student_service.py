44b173d60c7139cbc9a24ba80b0dcee8c9e3a257
profchaudhry <lectures.uni@gmail.com>
2026-02-24 21:29:49 +0500

    Update student_service.py

--- services/student_service.py
+++ services/student_service.py
@@ -1,15 +1,30 @@
+import streamlit as st
 from services.supabase_client import supabase
 from services.base_service import BaseService
 from config import CACHE_TTL
-import streamlit as st
+
 
 class StudentService(BaseService):
 
     @staticmethod
     @st.cache_data(ttl=CACHE_TTL)
     def get_profile(user_id):
-        return supabase.table("profiles")\
-            .select("*")\
-            .eq("id", user_id)\
-            .single()\
-            .execute().data
+
+        try:
+            response = (
+                supabase
+                .table("profiles")
+                .select("*")
+                .eq("id", user_id)
+                .execute()
+            )
+
+            data = response.data
+
+            if data and len(data) > 0:
+                return data[0]
+
+            return None
+
+        except Exception:
+            return None
