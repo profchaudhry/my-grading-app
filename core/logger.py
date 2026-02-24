48e708714bf8aae81dfc3d5035ec258bf1a2a9e7
profchaudhry <lectures.uni@gmail.com>
2026-02-25 02:02:58 +0500

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -2,5 +2,8 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    force=True,  # Ensures this config applies even if other libs init logging first
 )
+
+logger = logging.getLogger("sylemax")
